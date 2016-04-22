ff1a6b4e95b2c7c224d695f2fd602d30c7476a69
Ingrid Biery <ingridbiery@gmail.com>
2016-04-22 00:05:11 +0000

    first pass at devise. Requires authentication before viewing user info. Doesn't allow user to choose or change password. Doesn't allow user to request new password.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+if Rails.env.development?
+  ENV['DEFAULT_PASSWORD'] ||= 'password'
+else
+  raise 'Need to set ENV var "DEFAULT_PASSWORD"' if ENV['DEFAULT_PASSWORD'].nil?
+end
+
 User.destroy_all
 
-ingrid_biery = User.create!(first_name: 'Ingrid', last_name: 'Biery', email: 'ingridbiery@gmail.com')
-christine_davidson = User.create!(first_name: 'Christine', last_name: 'Davidson', email: 'christine.davidson@outlook.com')
+ingrid_biery = User.create!(password: ENV['DEFAULT_PASSWORD'], first_name: 'Ingrid', last_name: 'Biery', email: 'ingridbiery@gmail.com')
+christine_davidson = User.create!(password: ENV['DEFAULT_PASSWORD'], first_name: 'Christine', last_name: 'Davidson', email: 'christine.davidson@outlook.com')

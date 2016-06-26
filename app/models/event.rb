310ab10865b7641b74ef3f8680db9e4fe1482b21
Ingrid Biery <ingridbiery@gmail.com>
2016-06-26 15:57:40 +0000

    add time to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,12 @@ class Event < ActiveRecord::Base
   
   def self.add_events(token, public_cal, member_cal, form_info)
     form_info[:weeks].each do |week|
-      dt = week[:date].to_datetime
-      GoogleAPI.add_event(token, public_cal.googleid, week[:label], dt, dt)
-      public_cal.events.create!(title: week[:label], start: dt, end: dt)
+      start_dt = week[:date] + " 10:00"
+      start_dt = start_dt.to_datetime
+      end_dt = week[:date] + " 15:00"
+      end_dt = end_dt.to_datetime
+      GoogleAPI.add_event(token, public_cal.googleid, week[:label], start_dt, end_dt)
+      public_cal.events.create!(title: week[:label], start: start_dt, end: start_dt)
     end
   end
 end

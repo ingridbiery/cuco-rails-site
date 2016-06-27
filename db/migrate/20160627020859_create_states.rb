9dd2ce7fcbbaec0a66c9b9803bb317e993dceba7
Anon <anon@example.com>
2016-06-27 03:03:52 +0000

    Revert "Dropdown for state selection in new/edit family views"
    This reverts commit 678be81f5b9826fe46b298d82d3bdc02e25317c9.

--- db/migrate/20160627020859_create_states.rb
+++
@@ -1,9 +0,0 @@
-class CreateStates < ActiveRecord::Migration
-  def change
-    create_table :states, id: false do |t|
-      t.string :state_code
-      t.string :state_name
-    end
-    add_index :states, :state_code, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627020859) do
+ActiveRecord::Schema.define(version: 20160618024659) do
 
   create_table "calendars", force: :cascade do |t|
     t.string   "googleid"
@@ -62,13 +62,6 @@ ActiveRecord::Schema.define(version: 20160627020859) do
 
   add_index "people", ["family_id", "created_at"], name: "index_people_on_family_id_and_created_at"
   add_index "people", ["family_id"], name: "index_people_on_family_id"
-
-  create_table "states", id: false, force: :cascade do |t|
-    t.string "state_code"
-    t.string "state_name"
-  end
-
-  add_index "states", ["state_code"], name: "index_states_on_state_code", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"

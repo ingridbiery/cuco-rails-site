4636ca1479d294231be79c8a2d3669ca1a4db86d
Anon <anon@example.com>
2016-07-01 05:34:58 +0000

    working primary adult check, only one per family

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,16 @@
 module PeopleHelper
+
+  def primary_exists
+    people = @family.people.all
+
+    people.each do |person|
+      if person.primary_adult == true
+        primary_name = "#{person.first_name} #{person.last_name}"
+        return primary_name
+      end
+    end
+    
+    return ""
+    
+  end
 end

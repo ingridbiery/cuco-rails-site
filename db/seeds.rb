7eb02e6cb7eaa6520c2b3e44807c5343569d1815
Anon <anon@example.com>
2016-07-04 15:59:28 +0000

    Remove test session info from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,21 +25,10 @@ williams.people.create!(first_name: "Christopher", last_name: "Williams", dob: "
 williams.people.create!(first_name: "David", last_name: "Williams", dob: "2002/01/01".to_date, primary_adult: false, pronoun_id: 1)
 williams.people.create!(first_name: "Matthew", last_name: "Williams", dob: "2005/01/01".to_date, primary_adult: false, pronoun_id: 1)
 
-CucoSession.destroy_all
-fake_summer = CucoSession.create!(name: 'Fake Summer Session')
-
-summer_public_cal = fake_summer.calendars.create!(googleid: '5a5mn9c90f5eu786ocd3rvjkbs', members_only: false)
-summer_member_cal = fake_summer.calendars.create!(googleid: 'eu3jj82rsctenv5c4abvnlktb0', members_only: true)
-
-summer_public_cal.events.create!(title: 'Public Event', start: 1.week.from_now.at_noon, end: 1.week.from_now.at_noon+1.hour)
-
-summer_member_cal.events.create!(title: 'Member Event', start: 1.week.from_now.at_noon+1.day, end: 1.week.from_now.at_noon+1.day+1.hour)
-
 Pronoun.destroy_all
 Pronoun.create!(pronouns: "He/Him/His")
 Pronoun.create!(pronouns: "She/Her/Hers")
 Pronoun.create!(pronouns: "They/Them/Their")
-
 
 cuco_calendar = User.create!(password: ENV['DEFAULT_PASSWORD'], first_name: 'CUCO', last_name: 'Calendar', email: 'cucocalendar@gmail.com')
 CucoSession.destroy_all

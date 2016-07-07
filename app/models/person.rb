62c852fa3e456fd4c0f1a73bf65d7d1490a0040c
Anon <anon@example.com>
2016-07-07 01:06:09 +0000

    Add email validation to person add/edit form

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,6 +7,10 @@ class Person < ActiveRecord::Base
   validates :dob, presence: true
   validates :family_id, presence: true
   validates :pronoun_id, presence: true
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 
   def preferred_pronouns
     Pronoun.find(pronoun_id).preferred_pronouns

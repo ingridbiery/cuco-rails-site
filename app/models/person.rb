d65085a664e2e2565364f3ada3e427fa26101c49
Anon <anon@example.com>
2016-07-07 04:08:10 +0000

    Add phone validation to person add/edit form

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,7 +8,22 @@ class Person < ActiveRecord::Base
   validates :family_id, presence: true
   validates :pronoun_id, presence: true
 
+  VALID_PHONE_FORMAT = /\A(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?\z/
+  
+  after_validation :clean_phone_number
+
   def preferred_pronouns
     Pronoun.find(pronoun_id).preferred_pronouns
   end
+
+  protected
+
+    def clean_phone_number
+      self[:phone] = strip_bad_characters(:phone)
+    end
+
+    def strip_bad_characters(attr)
+      input = read_attribute_before_type_cast(attr)
+      input.gsub(/[^\d+!x]/, '') if input
+    end
 end

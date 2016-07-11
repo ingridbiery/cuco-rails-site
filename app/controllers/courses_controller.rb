dffab496e6c413bc9d1fb2525a0395608cfe77a6
Anon <anon@example.com>
2016-07-11 02:12:44 +0000

    Add courses#new and courses#edit views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,24 @@
 class CoursesController < ApplicationController
 
   def new
+    @course = Course.new
   end
+
+  def edit
+  end
+
+  private
+
+    def set_family
+      @course = Course.find(params[:id])
+    end
+
+    def family_params
+      params.require(:course).permit(:title, :short_title, :description, :min_age,
+                                     :max_age, :age_firm, :min_num_students,
+                                     :max_num_students, :course_fee, :supplies,
+                                     :room_requirements, :time_requirements,
+                                     :drop_ins, :additional_info)
+    end
 
 end

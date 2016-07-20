915fb8ac4c3f2c2333f90ff2ad736373f9814570
Anon <anon@example.com>
2016-07-20 02:30:07 +0000

    Attempt to fix course/room references

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ActiveRecord::Base
-  belongs_to :courses
+  has_many :courses
   validates :name, presence: true, uniqueness: { case_sensitive: false,
                                    message: "has already been added." }
 end

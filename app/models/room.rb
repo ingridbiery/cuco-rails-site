3be9b2c44471802f2f5c670d3a11f897771f2ebc
Anon <anon@example.com>
2016-07-20 02:12:47 +0000

    Add validation to Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ActiveRecord::Base
   belongs_to :courses
+  validates :name, presence: true, uniqueness: { case_sensitive: false,
+                                   message: "has already been added." }
 end

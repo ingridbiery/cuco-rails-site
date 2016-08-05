21dd05bc6757154037f01cd723f2cdba73ef6d5e
Anon <anon@example.com>
2016-08-05 04:34:15 +0000

    Add Room and Period to Course views

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,10 @@
 class Course < ActiveRecord::Base
   belongs_to :cuco_session
   has_one :room
+
+  # allow one to use @course.room in views
+  def room
+    Room.find(assigned_room).name
+  end
+  
 end

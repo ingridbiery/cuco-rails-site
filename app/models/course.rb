1067ca7972749e15f4a934efff7ba4328991eac0
Ingrid Biery <ingridbiery@gmail.com>
2016-08-10 17:36:13 +0000

    belongs_to :room means we can use course.room without having to define it ourselves.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,13 +2,4 @@ class Course < ActiveRecord::Base
   belongs_to :cuco_session
   belongs_to :room
   belongs_to :period
-
-  # allow one to use @course.room in views
-  def room
-    Room.find(room_id).name
-  end
-  
-  def period
-    Period.find(period_id).name
-  end
 end

3850ff597c90f298e7ca3c847736cf3ce6dc7286
Anon <anon@example.com>
2016-08-16 21:13:37 +0000

    Update course fees on forms and add tests/validations to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,10 +4,9 @@ class Course < ActiveRecord::Base
   belongs_to :period
 
   validates :title, presence: true,
-                    length: { minimum: 5, maximum: 75 }
+                    length: { maximum: 100 }
 
-  validates :short_title, presence: true,
-                    length: { minimum: 3, maximum: 15 }
+  validates :short_title, length: { maximum: 30 }
 
   validates :description, presence: true
 
@@ -17,7 +16,7 @@ class Course < ActiveRecord::Base
   validates :min_students, length: { maximum: 3 }, numericality: true
   validates :max_students, length: { maximum: 3 }, numericality: true
 
-  validates :fee, length: { maximum: 5 }, numericality: true, allow_blank: true
+  validates :fee, length: { maximum: 3 }, numericality: true, allow_blank: true
 
   validate :age_ranges
   validate :student_ranges
@@ -26,14 +25,14 @@ class Course < ActiveRecord::Base
   
   # Check to see if min_age is less than max_age
   def age_ranges
-    if max_age.to_i < min_age.to_i
+    if max_age.to_i <= min_age.to_i
       errors.add("Max age", "(#{max_age}) is not greater than min age (#{min_age})")
     end
   end
   
   # Check to see if min_students is less than max_students
   def student_ranges
-    if max_students.to_i < min_students.to_i
+    if max_students.to_i <= min_students.to_i
       errors.add("Max students", "(#{max_students}) is not greater than min students (#{min_students})")
     end
   end

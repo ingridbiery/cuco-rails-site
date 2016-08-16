32d59c959a2dd951040d38101f331dec76709e79
Anon <anon@example.com>
2016-08-16 15:27:33 +0000

    Add model validations for age and student ranges

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,10 +4,10 @@ class Course < ActiveRecord::Base
   belongs_to :period
 
   validates :title, presence: true,
-                    length: { maximum: 75 }
+                    length: { minimum: 5, maximum: 75 }
 
   validates :short_title, presence: true,
-                    length: { maximum: 15 }
+                    length: { minimum: 3, maximum: 15 }
 
   validates :description, presence: true
 
@@ -19,4 +19,23 @@ class Course < ActiveRecord::Base
 
   validates :fee, length: { maximum: 5 }, numericality: true, allow_blank: true
 
+  validate :age_ranges
+  validate :student_ranges
+
+  private
+  
+  # Check to see if min_age is less than max_age
+  def age_ranges
+    if max_age.to_i < min_age.to_i
+      errors.add("Max age", "(#{max_age}) is not greater than min age (#{min_age})")
+    end
+  end
+  
+  # Check to see if min_students is less than max_students
+  def student_ranges
+    if max_students.to_i < min_students.to_i
+      errors.add("Max students", "(#{max_students}) is not greater than min students (#{min_students})")
+    end
+  end
+  
 end

4d23775dfd2655beb21db1701941836336e8ad4b
Anon <anon@example.com>
2016-08-20 04:17:21 +0000

    Add phone number validation to family model

--- app/models/family.rb
+++ app/models/family.rb
@@ -20,12 +20,18 @@ class Family < ActiveRecord::Base
                             length: { maximum: 50 }
   validates :ec_last_name, presence: true,
                             length: { maximum: 50 }
-  validates :ec_phone, presence: true,
-                       numericality: true
   validates :ec_text, presence: true
   validates :ec_relationship, presence: true,
                               length: { maximum: 50 }
 
+
+  # Based on https://codereview.stackexchange.com/questions/60171/refactoring-complex-phone-validations
+  
+  VALID_PHONE_FORMAT = /\A(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?\z/
+  validates :ec_phone, format: { with: VALID_PHONE_FORMAT },
+                       presence: true
+
+  after_validation :clean_phone_number
 
   # get the person object for the primary adult of this family         
   def primary_adult
@@ -42,4 +48,24 @@ class Family < ActiveRecord::Base
     return nil
   end
   
+  protected
+  
+   def clean_phone_number
+     # As long as there is a valid phone number in the field, the following
+     # will call the strip_bad_characters function below on the phone number
+     # entered into that field.
+     unless self.errors.any?
+       self[:ec_phone] = strip_bad_characters(:ec_phone)
+     end
+   end
+  
+   def strip_bad_characters(attr)
+     # This will strip any parentheses, dashes, and spaces from the phone number
+     # leaving only a 10-digit integer. This 10-digit integer will then be added
+     # to the database and accessible as @person.phone. See the person#show view
+     # for an example of how to format this into a more human-readable format.
+     input = read_attribute_before_type_cast(attr)
+     input.gsub(/[^\d+!x]/, '') if input
+   end
+
 end

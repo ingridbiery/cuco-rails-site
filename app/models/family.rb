91019be3a701f9cda5fe45d6db5265fec7213d13
Anon <anon@example.com>
2016-08-24 03:20:18 +0000

    Add tests to family model tests for ec and letters only

--- app/models/family.rb
+++ app/models/family.rb
@@ -5,24 +5,32 @@ class Family < ActiveRecord::Base
   has_many :kids, -> { where.not(dob: nil) }, class_name: 'Person', foreign_key: :family_id
   has_many :adults, -> { where(dob: nil) }, class_name: 'Person', foreign_key: :family_id
   default_scope -> { order(name: :asc) }
+  
+  ONLY_LETTERS = /\A[a-zA-Z ]+$\z/
+  ONLY_LETTERS_MESSAGE = "can only contain letters."
+  
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false },
-                   length: { maximum: 50 }
+                   length: { maximum: 50 },
+                   format: { with: ONLY_LETTERS, message: ONLY_LETTERS_MESSAGE }
   validates :street_address, presence: true
   validates :city, presence: true,
-                   length: { maximum: 50 }
+                   length: { maximum: 50 },
+                   format: { with: ONLY_LETTERS, message: ONLY_LETTERS_MESSAGE }
   validates :state, presence: true,
                     length: { minimum: 2, maximum: 2 }
   validates :zip, presence: true,
                   length: { minimum: 5, maximum: 5 },
                   numericality: true
   validates :ec_first_name, presence: true,
-                            length: { maximum: 50 }
+                            length: { maximum: 50 },
+                            format: { with: ONLY_LETTERS, message: ONLY_LETTERS_MESSAGE }
   validates :ec_last_name, presence: true,
-                            length: { maximum: 50 }
-  validates :ec_text, presence: true, allow_blank: true
+                            length: { maximum: 50 },
+                            format: { with: ONLY_LETTERS, message: ONLY_LETTERS_MESSAGE }
   validates :ec_relationship, presence: true,
-                              length: { maximum: 50 }
+                              length: { maximum: 50 },
+                              format: { with: ONLY_LETTERS, message: ONLY_LETTERS_MESSAGE }
 
 
   # Based on https://codereview.stackexchange.com/questions/60171/refactoring-complex-phone-validations
--- test/models/family_test.rb
+++ test/models/family_test.rb
@@ -8,29 +8,11 @@ class FamilyTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @family.valid?
   end
-  
+
+#### name ####
+
   test "name should be present" do
     @family.name = nil
-    assert_not @family.valid?
-  end
-
-  test "street_address should be present" do
-    @family.street_address = nil
-    assert_not @family.valid?
-  end
-
-  test "city should be present" do
-    @family.city = nil
-    assert_not @family.valid?
-  end
-
-  test "state should be present" do
-    @family.state = nil
-    assert_not @family.valid?
-  end
-
-  test "zip should be present" do
-    @family.zip = nil
     assert_not @family.valid?
   end
 
@@ -46,8 +28,39 @@ class FamilyTest < ActiveSupport::TestCase
     assert_not duplicate_family.valid?
   end
 
+  test "name should only contain letters" do
+    @family.name = "Smith1"
+    assert_not @family.valid?
+  end
+
+#### street_address ####
+
+  test "street_address should be present" do
+    @family.street_address = nil
+    assert_not @family.valid?
+  end
+
+#### city ####
+
+  test "city should be present" do
+    @family.city = nil
+    assert_not @family.valid?
+  end
+
   test "city should not be too long" do
     @family.city = "a" * 51
+    assert_not @family.valid?
+  end
+
+  test "city should only contain letters" do
+    @family.city = "Columbus1"
+    assert_not @family.valid?
+  end
+
+#### state ####
+
+  test "state should be present" do
+    @family.state = nil
     assert_not @family.valid?
   end
 
@@ -58,6 +71,13 @@ class FamilyTest < ActiveSupport::TestCase
 
   test "state should not be too long" do
     @family.state = "a" * 3
+    assert_not @family.valid?
+  end
+
+#### zip ####
+
+  test "zip should be present" do
+    @family.zip = nil
     assert_not @family.valid?
   end
 
@@ -73,6 +93,64 @@ class FamilyTest < ActiveSupport::TestCase
 
   test "zip should not be too long" do
     @family.zip = "1" * 6
+    assert_not @family.valid?
+  end
+
+#### ec_first_name ####
+
+  test "ec_first_name should be present" do
+    @family.ec_first_name = nil
+    assert_not @family.valid?
+  end
+
+  test "ec_first_name should not be too long" do
+    @family.ec_first_name = "1" * 51
+    assert_not @family.valid?
+  end
+
+  test "ec_first_name should only contain letters" do
+    @family.ec_first_name = "Jennifer3"
+    assert_not @family.valid?
+  end
+
+#### ec_last_name ####
+
+  test "ec_last_name should be present" do
+    @family.ec_last_name = nil
+    assert_not @family.valid?
+  end
+
+  test "ec_last_name should not be too long" do
+    @family.ec_last_name = "1" * 51
+    assert_not @family.valid?
+  end
+
+  test "ec_last_name should only contain letters" do
+    @family.ec_first_name = "Smith4"
+    assert_not @family.valid?
+  end
+
+#### ec_phone ####
+
+  test "ec_phone should be present" do
+    @family.ec_phone = nil
+    assert_not @family.valid?
+  end
+
+#### ec_relationship ####
+
+  test "ec_relationship should be present" do
+    @family.ec_relationship = nil
+    assert_not @family.valid?
+  end
+
+  test "ec_relationship should not be too long" do
+    @family.ec_first_name = "1" * 51
+    assert_not @family.valid?
+  end
+
+  test "ec_relationship should only contain letters" do
+    @family.ec_relationship = "Father5"
     assert_not @family.valid?
   end
 

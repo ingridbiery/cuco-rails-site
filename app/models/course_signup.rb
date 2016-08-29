4db57980bf7e897a851b3ea54f1962ca25cb3b62
Ingrid Biery <ingridbiery@gmail.com>
2016-08-29 18:07:14 +0000

    validate course capacity and student age (with warning for suggested age) for course signup

--- app/models/course_signup.rb
+++ app/models/course_signup.rb
@@ -1,4 +1,42 @@
 class CourseSignup < ActiveRecord::Base
+  include ActiveWarnings
+  
   belongs_to :course
   belongs_to :person
+  
+  validate :course_capacity
+  validate :student_age_firm
+
+  def course_capacity
+    if course.max_students == course.people.count
+      errors.add("Max students", "has been reached")
+    end
+  end
+  
+  def student_age_firm
+    if course.age_firm
+      student_age
+    end
+  end
+  
+  def student_age
+    if person.age != nil
+      if person.age < course.min_age
+        errors.add("Student", "too young for course")
+      elsif person.age > course.max_age
+        errors.add("Student", "too old for course")
+      end
+    end
+  end
+
+  warnings do
+    validate :student_age_suggestion
+
+    def student_age_suggestion
+      if !course.age_firm
+        student_age
+      end
+    end
+  end
+
 end

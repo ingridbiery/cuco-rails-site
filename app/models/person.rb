45278a485cbbc0c9495e0a7dbde841f17c7ea667
Ingrid Biery <ingridbiery@gmail.com>
2016-10-11 02:00:00 +0000

    use scope to isolate compartmentalize information

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,6 +5,9 @@ class Person < ActiveRecord::Base
   has_many :course_signups, dependent: :destroy
   has_many :courses, through: :course_signups
 
+  scope :kid, -> { where.not(dob: nil) }
+  scope :adult, -> { where(dob: nil) }
+  
   default_scope -> { order(first_name: :asc) }
 
   validates_each :first_name, :last_name do |record, attr, value|

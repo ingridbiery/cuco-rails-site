e103c811cb490a00b46cfe40c2ab00d50a46b7ea
Anon <anon@example.com>
2016-12-21 03:58:05 +0000

    Add check for pronoun preference

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,6 +7,7 @@ class MembershipsController < ApplicationController
   before_action :set_cuco_session, except: :paypal_hook
   before_action :set_membership, only: :show
   before_action :must_be_own_schedule, only: :show_schedule
+  before_action :pronoun_preference_must_be_updated, only: [:new, :create]
   
   # the paypal hook gets called with no user and needs access that we wouldn't normally grant
   skip_before_action :authenticate, only: :paypal_hook
@@ -70,6 +71,16 @@ class MembershipsController < ApplicationController
         not_authorized! message: "That's not your schedule."
       end
       
+    end
+
+    def pronoun_preference_must_be_updated
+      current_user&.person&.family&.people.each do |person|
+        unless person.pronoun_id?
+          redirect_to family_path(current_user&.person&.family.id), notice: "Please update your family's pronoun preferences before continuing."
+          return
+        end
+      end
+ 
     end
 
     # get the cuco_session from params before doing anything else

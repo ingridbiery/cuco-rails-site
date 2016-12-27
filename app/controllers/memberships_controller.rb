3b9ccc41af234ef8f6b4095b6a588dbbdf8995e3
Ingrid Biery <ingridbiery@gmail.com>
2016-12-27 01:04:18 +0000

    fix incorrect merge -- validation moved to model, warnings used for invalid people in family

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,7 +7,7 @@ class MembershipsController < ApplicationController
   before_action :set_cuco_session, except: :paypal_hook
   before_action :set_membership, only: :show
   before_action :must_be_own_schedule, only: :show_schedule
-  before_action :family_info_must_be_updated, only: [:new, :create]
+  before_action :family_info_must_be_correct, only: [:new, :create]
   
   # the paypal hook gets called with no user and needs access that we wouldn't normally grant
   skip_before_action :authenticate, only: :paypal_hook
@@ -55,21 +55,6 @@ class MembershipsController < ApplicationController
         not_authorized! message: "That's not your schedule."
       end
       
-    end
-
-    def family_info_must_be_updated
-      current_user&.person&.family&.people.each do |person|
-        if !person.pronoun_id?
-          redirect_to family_path(current_user.person.family),
-                      notice: "Please update your family's pronoun preferences before continuing."
-          return
-        end
-        if person.family.phone == "6145551212"
-          redirect_to family_path(current_user.person.family),
-                      notice: "Please update your family's phone number before continuing."
-          return
-        end
-      end
     end
 
     # get the cuco_session from params before doing anything else

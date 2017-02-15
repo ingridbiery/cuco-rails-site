87260220a8da1f1f2b8f026b0e193c8476deb0c7
Ingrid Biery <ingridbiery@gmail.com>
2017-02-15 17:56:01 +0000

    put note in empty test

--- test/models/family_schedule_test.rb
+++ test/models/family_schedule_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class FamilyScheduleTest < ActiveSupport::TestCase
+  test "empty" do
+    print "\nFamilyScheduleTest empty\n"
+  end
   # test "the truth" do
   #   assert true
   # end

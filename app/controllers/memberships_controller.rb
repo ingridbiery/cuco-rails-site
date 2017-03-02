5b80d0e8fcf3e1a2b8274dbe70016a5fc9836223
Ingrid Biery <ingridbiery@gmail.com>
2017-03-02 03:32:18 +0000

    fix bug relating to familyschedule

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -18,7 +18,7 @@ class MembershipsController < ApplicationController
     if status == "Completed"
       @membership = Membership.find params[:invoice]
       @membership.update_attributes! notification_params: params, status: status, transaction_id: params[:txn_id], purchased_at: Time.now
-      FamilySchedule.create(@membership.attributes)
+      FamilySchedule.create(cuco_session: @membership.cuco_session, family: @membership.family)
     end
     render nothing: true
   end
